e595729b641ff30fa95f63b0e0d9b0dbc7b15757
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-04-13 01:16:16 +0000

    in-class exercise

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,28 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number INTEGER,
+    company_id INTEGER
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    activity_type TEXT,
+    notes TEXT,
+    date_time TEXT,
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
+);
+
+CREATE TABLE internals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
+);
